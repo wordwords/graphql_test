6f25c73ce48d83931c685dd558342351e24443e5
wordword <ggavi2000@naver.com>
2020-09-18 14:32:48 +0900

    주석수정

--- graphql/db.js
+++ graphql/db.js
@@ -1,17 +1,16 @@
 import fetch from "node-fetch";
 const API_URL = "https://api.manana.kr/calendar.json";
 
-export const getDay = () => {
 
-    // 저 API 주소를 주소창에 입력하면 JSON 데이터가 뜨는데, 그게 어떤 형태로 들어오는지 콘솔 로그로 확인 필요 (상황에 따라 json => json.data 이런 식으로 작성해야 한다)
+// [예시1] 만약 외부 RESTful API를 가져오는 경우 (이번달 매주 월요일 데이터 가져오기)
+export const getDay = () => {
     return fetch(`${API_URL}`)
         .then(res => res.json())
         .then(json => json)
 }
 
 
-// 이 부분을 데이터베이스에서 가져온 데이터로 교체해야 함
-// people에 해당하는 array를 만들자
+// [예시2] 만약 데이터베이스에서 이런 형태로 데이터 가져온 경우
 let movies = [
     {
         id: 0,
@@ -31,15 +30,17 @@ let movies = [
 
 ]; 
 
-// 전체출력
+
+// DB에서 가져온 데이터 전체출력 예시
 export const getMovies = () => movies;
 
-// people 배열 안에 있는 모든 Object 확인후 조건에 맞는걸 return
+// DB에서 가져온 데이터 특정값 탐색 예시 (people 배열 안에 있는 모든 Object 확인후 조건에 맞는걸 return)
 export const getById = id => {
     const filteredMovies = people.filter(movie => movie.id === id);
     return filteredMovies[0]
 }
 
+// DB의 데이터 삭제 예시
 export const deleteMovie = id => {
     const cleanedMovies = movies.filter(movie => movie.id !== id);
     if(movies.length > cleanedMovies.length) {
@@ -50,6 +51,7 @@ export const deleteMovie = id => {
     }
 }
 
+// DB에 데이터 추가 예시
 export const addMovie = (name, score) => {
     const newMovie = {
         id: movies.length + 1,
